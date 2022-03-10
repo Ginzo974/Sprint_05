da40863f8ada433c36f34353ce622fb4d33f3160
Ginzo974 <quentin.louis97400@gmail.com>
2022-03-10 17:30:37 +0400

    la modif ultime

--- main.js
+++ main.js
@@ -2,12 +2,37 @@
 //   .then((res) => res.json())
 //   .then((resp2) => console.log(resp2));
 async function render() {
-  let cons = await fetch("https://restcountries.com/v3.1/region/europe");
-  let rep = await cons.json();
-  let apparition = "";
-  for (let i in rep) {
-    apparition += `<li>${rep[i].name.official}</li>`;
-    document.getElementById("li").innerHTML = apparition;
+  try {
+    let cons = await fetch("https://restcountries.com/v3.1/region/europe");
+    if (cons.ok) {
+      let rep = await cons.json();
+      let apparition = "";
+      for (let i in rep) {
+        apparition += `<li>${rep[i].name.official}</li>`;
+      }
+      let btn = document.createElement("button");
+      let text = document.createTextNode("refresh");
+      btn.appendChild(text);
+      document.getElementById("rafri").textContent = "";
+      document.getElementById("rafri").appendChild(btn);
+      btn.addEventListener("click", function () {
+        render();
+      });
+      document.getElementById("li").innerHTML = apparition;
+      console.log("ça marche comme sur des roulettes :D");
+    }
+  } catch (e) {
+    console.error(e);
+    console.log("dans l'erreur");
+    let btn = document.createElement("button");
+    let text = document.createTextNode("refresh");
+    btn.appendChild(text);
+    document.getElementById("rafri").textContent = "";
+    document.getElementById("rafri").appendChild(btn);
+    btn.addEventListener("click", function () {
+      render();
+    });
+    console.log("ça marche plus x(");
   }
 }
 
--- table.js
+++ table.js
@@ -1,17 +1,41 @@
 async function render() {
-  let reponse = await fetch("https://restcountries.com/v3.1/region/europe");
-  let recup = await reponse.json();
-  let afficher = "";
-  for (let i in recup) {
-    afficher += `
+  try {
+    let reponse = await fetch("https://restcountries.com/v3.1/region/europe");
+    if (reponse.ok) {
+      let recup = await reponse.json();
+      let afficher = "";
+      for (let i in recup) {
+        afficher += `
       <tr class = text-align>
         <td class = fw-bold>${recup[i].name.official}</td> <td class = "text-end">${recup[i].area}</td>
         <td class = "text-end">${recup[i].population}</td>
         <td>${recup[i].capital}</td>
       </tr>
     `;
-    console.log(i);
-    document.getElementById("tab").innerHTML = afficher;
+      }
+      let btn = document.createElement("button");
+      let text = document.createTextNode("refresh table");
+      btn.appendChild(text);
+      document.getElementById("rafri").textContent = "";
+      document.getElementById("rafri").appendChild(btn);
+      btn.addEventListener("click", function () {
+        render();
+      });
+      document.getElementById("tab").innerHTML = afficher;
+      console.log("ça marche comme sur des roulettes :D");
+    }
+  } catch (e) {
+    console.error(e);
+    console.log("dans l'erreur");
+    let btn = document.createElement("button");
+    let text = document.createTextNode("refresh table");
+    btn.appendChild(text);
+    document.getElementById("rafri").textContent = "";
+    document.getElementById("rafri").appendChild(btn);
+    btn.addEventListener("click", function () {
+      render();
+    });
+    console.log("ça marche plus x(");
   }
 }
 render();
